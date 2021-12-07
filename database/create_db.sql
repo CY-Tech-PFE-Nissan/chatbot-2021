19576dc5bd681012561f023beaf5abcb443e973d
Ryan HOUSSENE <houssenery@eisti.eu>
2021-12-07 11:48:18 +0100

    Add chatbot_order in create_db.sql

--- database/create_db.sql
+++ database/create_db.sql
@@ -10,6 +10,18 @@ CREATE TABLE chatbot_question
     topic_terms varchar(250)
 );
 
+CREATE TABLE chatbot_order
+(
+    id serial primary key,
+    topic varchar(50),
+    sub_topic varchar(50),
+    video_title varchar(50),
+    question varchar(250),
+    answer varchar(2000),
+    sequence_tree varchar(1000),
+    topic_terms varchar(250)
+);
+
 CREATE TABLE chatbot_video
 (
     id serial primary key,
